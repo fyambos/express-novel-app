9c2dfaef98e018c47a81a88479ac97fcdba9514f
fyambos <feliciteyambos@gmail.com>
2024-12-28 15:41:20 +0100

    tech: add backend endpoints to front end service

--- src/app/services/chapter.service.ts
+++ src/app/services/chapter.service.ts
@@ -6,19 +6,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ChapterService {
-  private baseUrl = 'http://localhost:5000/api/chapters';
+  private baseUrl = 'http://localhost:5000/api';
 
   constructor(private http: HttpClient) {}
 
   createChapter(chapter: any): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}`, chapter);
+    return this.http.post<any>(`${this.baseUrl}/chapters`, chapter);
   }
 
   getChapterById(chapterId: string): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/${chapterId}`);
+    return this.http.get<any>(`${this.baseUrl}/chapters/${chapterId}`);
   }
 
   editChapter(chapterId: string, chapter: any): Observable<any> {
-    return this.http.put<any>(`${this.baseUrl}/${chapterId}`, chapter);
+    return this.http.put<any>(`${this.baseUrl}/chapters/${chapterId}`, chapter);
+  }
+
+  deleteChapter(chapterId: string): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/chapters/${chapterId}`);
+  }
+
+  getChaptersByStoryId(storyId: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/stories/${storyId}/chapters`);
   }
 }
