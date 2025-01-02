14698c7d0bc40b20c01928289a7fb212a3338f92
fyambos <feliciteyambos@gmail.com>
2025-01-02 08:12:27 +0100

    chore: clean up comment component

--- src/app/pages/comment/comment.component.ts
+++ src/app/pages/comment/comment.component.ts
@@ -2,11 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Auth, onAuthStateChanged, User } from '@angular/fire/auth';
-import { MatDialog } from '@angular/material/dialog';
 import { Comment } from 'src/app/models/comment.model';
 import { CommentService } from 'src/app/services/comment.service';
-import { AddCommentDialogComponent } from 'src/app/components/add-comment-dialog/add-comment-dialog.component';
-import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-comment',
@@ -20,11 +17,8 @@ export class CommentComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
     private auth: Auth,
-    private dialog: MatDialog,
     private commentService: CommentService,
-    private userService: UserService,
   ) {}
 
   async ngOnInit() {
@@ -33,7 +27,6 @@ export class CommentComponent implements OnInit {
       if (commentId) {
         this.checkCurrentUser();
         this.comments = await this.commentService.getCommentById(commentId);
-        console.log("this.comments", this.comments);
       }
     });
   }
@@ -47,24 +40,6 @@ export class CommentComponent implements OnInit {
           this.isLoading = false;
         }
       });
-  }
-
-    async openCommentModal() {
-    if (!this.currentUserUid) {
-      this.router.navigate(['/login']);
-      return;
-    }
-    const dialogRef = this.dialog.open(AddCommentDialogComponent, {
-      width: '400px',
-      data: { chapterId: this.comments[0].chapterId, authorId: this.currentUserUid },
-    });
-
-    dialogRef.afterClosed().subscribe(async (newComment) => {
-      if (newComment) {
-        newComment.author = await this.userService.fetchUser(newComment.authorId);
-        this.comments.push(newComment);
-      }
-    });
   }
 
 }
